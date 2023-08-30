45debb6ffbe25df7d4162c2ebb5659916d13e715
YingzhePeng <yingzhepeng@foxmail.com>
2023-08-30 21:06:21 +0800

    format

--- openicl/icl_inferencer/icl_flamingo_gen_inferencer.py
+++ openicl/icl_inferencer/icl_flamingo_gen_inferencer.py
@@ -1,19 +1,24 @@
-from typing import List, Optional, Dict, Union
+import json
+from typing import Dict, List, Optional, Union
 
 import more_itertools
-import json
 import torch
 from accelerate import Accelerator
+from openicl import PromptTemplate
+from openicl.icl_inferencer.icl_base_inferencer import (
+    BaseInferencer,
+    GenInferencerOutputHandler,
+)
+from openicl.icl_retriever import *
+from openicl.utils.icl_common_utils import (
+    get_generation_prompt_list_from_retriever_indices,
+    get_generation_vision_x_from_retriever_indices,
+)
+from openicl.utils.logging import get_logger
 from tqdm import tqdm
 from transformers import PretrainedConfig
-from datasets import Dataset
 
-from openicl import PromptTemplate
-from openicl.icl_inferencer.icl_base_inferencer import BaseInferencer, GenInferencerOutputHandler
-from openicl.icl_retriever import *
-from openicl.utils.icl_common_utils import get_generation_prompt_list_from_retriever_indices, \
-    get_generation_vision_x_from_retriever_indices
-from openicl.utils.logging import get_logger
+from datasets import Dataset
 
 logger = get_logger(__name__)
 
@@ -25,9 +30,7 @@ class FlamingoGenInferencerOutputHandler(GenInferencerOutputHandler):
     results_dict = {}
     origin_image_dict = {}
 
-    def __init__(self,
-                 num: int,
-                 accelerator: Optional[Accelerator] = None):
+    def __init__(self, num: int, accelerator: Optional[Accelerator] = None):
         super().__init__(num, accelerator)
         self.other_meta_info_dict = {}
 
@@ -36,29 +39,41 @@ class FlamingoGenInferencerOutputHandler(GenInferencerOutputHandler):
         meta_list = test_ds[meta_field]
         for idx, m_d in enumerate(meta_list):
             if self.accelerator is not None:
-                idx = idx * self.accelerator.num_processes + self.accelerator.process_index
+                idx = (
+                    idx * self.accelerator.num_processes
+                    + self.accelerator.process_index
+                )
             meta_dict[str(idx)] = m_d
         self.other_meta_info_dict[meta_field] = meta_dict
 
-    def subprocess_write_to_json(self, output_json_filepath: str, output_json_filename: str):
+    def subprocess_write_to_json(
+        self, output_json_filepath: str, output_json_filename: str
+    ):
         self.results_dict = {
             str(idx): {
                 'origin_prompt': self.origin_prompt_dict[str(idx)],
                 'output': self.output_dict[str(idx)],
                 'prediction': self.prediction_dict[str(idx)],
-            } for idx in self.origin_prompt_dict.keys()
+            }
+            for idx in self.origin_prompt_dict.keys()
         }
         for field in self.other_meta_info_dict:
             for idx in self.origin_prompt_dict.keys():
                 if field in self.results_dict[str(idx)]:
                     logger.warning(
-                        'the other meta info field name has duplicate! Please check for avoiding to losing info')
+                        'the other meta info field name has duplicate! Please check for avoiding to losing info'
+                    )
                     continue
-                self.results_dict[str(idx)][field] = self.other_meta_info_dict[field][str(idx)]
+                self.results_dict[str(idx)][field] = self.other_meta_info_dict[field][
+                    str(idx)
+                ]
 
         if self.accelerator is not None:
-            with open(f'{output_json_filepath}/process{self.accelerator.process_index}_{output_json_filename}.json',
-                      'w', encoding='utf-8') as json_file:
+            with open(
+                f'{output_json_filepath}/process{self.accelerator.process_index}_{output_json_filename}.json',
+                'w',
+                encoding='utf-8',
+            ) as json_file:
                 json.dump(self.results_dict, json_file, indent=4, ensure_ascii=False)
                 json_file.close()
 
@@ -81,27 +96,39 @@ class FlamingoGenInferencer(BaseInferencer):
         generation_kwargs (:obj:`Dict`, optional): Parameters for the :obj:`model.generate()` method.
     """
 
-    def __init__(self,
-                 model_name: Optional[str] = 'gpt2-xl',
-                 tokenizer_name: Optional[str] = None,
-                 image_processor=None,
-                 image_field='',
-                 other_save_field: Optional[List] = None,
-                 autocast_context=None,
-                 max_model_token_num: Optional[int] = None,
-                 model_config: Optional[PretrainedConfig] = None,
-                 batch_size: Optional[int] = 1,
-                 gen_field_replace_token: Optional[str] = '',
-                 generation_kwargs={"max_new_tokens": 100},
-                 accelerator: Optional[Accelerator] = None,
-                 output_json_filepath: Optional[str] = "./icl_inference_output",
-                 output_json_filename: Optional[str] = "predictions",
-                 api_name: Optional[str] = None,
-                 model_parallel: Optional[bool] = False,
-                 **kwargs
-                 ) -> None:
-        super().__init__(model_name, tokenizer_name, max_model_token_num, model_config, batch_size, accelerator,
-                         output_json_filepath, output_json_filename, api_name, model_parallel, **kwargs)
+    def __init__(
+        self,
+        model_name: Optional[str] = 'gpt2-xl',
+        tokenizer_name: Optional[str] = None,
+        image_processor=None,
+        image_field='',
+        other_save_field: Optional[List] = None,
+        autocast_context=None,
+        max_model_token_num: Optional[int] = None,
+        model_config: Optional[PretrainedConfig] = None,
+        batch_size: Optional[int] = 1,
+        gen_field_replace_token: Optional[str] = '',
+        generation_kwargs={"max_new_tokens": 100},
+        accelerator: Optional[Accelerator] = None,
+        output_json_filepath: Optional[str] = "./icl_inference_output",
+        output_json_filename: Optional[str] = "predictions",
+        api_name: Optional[str] = None,
+        model_parallel: Optional[bool] = False,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            model_name,
+            tokenizer_name,
+            max_model_token_num,
+            model_config,
+            batch_size,
+            accelerator,
+            output_json_filepath,
+            output_json_filename,
+            api_name,
+            model_parallel,
+            **kwargs,
+        )
         self.gen_field_replace_token = gen_field_replace_token
         self.generation_kwargs = generation_kwargs
         self.image_processor = image_processor
@@ -110,9 +137,16 @@ class FlamingoGenInferencer(BaseInferencer):
         self.autocast_context = autocast_context
 
     @torch.inference_mode()
-    def inference(self, retriever: BaseRetriever, ice_template: Optional[PromptTemplate] = None,
-                  prompt_template: Optional[PromptTemplate] = None, output_json_filepath: Optional[str] = None,
-                  output_json_filename: Optional[str] = None, force_words=None, return_dict=False) -> Union[List, Dict]:
+    def inference(
+        self,
+        retriever: BaseRetriever,
+        ice_template: Optional[PromptTemplate] = None,
+        prompt_template: Optional[PromptTemplate] = None,
+        output_json_filepath: Optional[str] = None,
+        output_json_filename: Optional[str] = None,
+        force_words=None,
+        return_dict=False,
+    ) -> Union[List, Dict]:
         # 1. Preparation for output logs
         num = len(retriever.test_ds)
         output_handler = FlamingoGenInferencerOutputHandler(num, self.accelerator)
@@ -128,52 +162,72 @@ class FlamingoGenInferencer(BaseInferencer):
 
         # 3. Generate prompts for testing input
         logger.info('begin concat the prompt...')
-        prompt_list = get_generation_prompt_list_from_retriever_indices(ice_idx_list, retriever, self.tokenizer,
-                                                                        self.gen_field_replace_token,
-                                                                        max_model_token_num=self.max_model_token_num,
-                                                                        ice_template=ice_template,
-                                                                        prompt_template=prompt_template)
+        prompt_list = get_generation_prompt_list_from_retriever_indices(
+            ice_idx_list,
+            retriever,
+            self.tokenizer,
+            self.gen_field_replace_token,
+            max_model_token_num=self.max_model_token_num,
+            ice_template=ice_template,
+            prompt_template=prompt_template,
+        )
         output_handler.save_orgin_prompts(prompt_list)
         for fields in self.other_save_field:
             output_handler.save_origin_info(fields, retriever.test_ds)
 
         # 4. Inference for prompts in each batch
         logger.info("Starting inference process...")
-        for idx_list in more_itertools.chunked(tqdm(range(len(prompt_list)), disable=not self.is_main_process),
-                                               self.batch_size):
+        for idx_list in more_itertools.chunked(
+            tqdm(range(len(prompt_list)), disable=not self.is_main_process),
+            self.batch_size,
+        ):
             text_entry = [prompt_list[i] for i in idx_list]
             sub_ice_idx_list = [ice_idx_list[idx] for idx in idx_list]
-            vision_x = get_generation_vision_x_from_retriever_indices(idx_list, sub_ice_idx_list, retriever,
-                                                                      self.image_processor,
-                                                                      self.image_field).to(self.device)
+            vision_x = get_generation_vision_x_from_retriever_indices(
+                idx_list,
+                sub_ice_idx_list,
+                retriever,
+                self.image_processor,
+                self.image_field,
+            ).to(self.device)
             # 5-1. Inference with local model
             with self.autocast_context:
                 tokenized_data = self.tokenizer.batch_encode_plus(
-                    text_entry, padding=True, return_tensors='pt').to(self.device)
+                    text_entry, padding=True, return_tensors='pt'
+                ).to(self.device)
 
                 prompt_len = int(tokenized_data.attention_mask.shape[1])
                 if force_words is not None:
                     force_words_ids = [
                         self.tokenizer(force_words).input_ids,
                     ]
-                    outputs = self.model.generate(vision_x=vision_x,
-                                                  lang_x=tokenized_data.input_ids,
-                                                  attention_mask=tokenized_data.attention_mask,
-                                                  force_words_ids=force_words_ids,
-                                                  num_beams=10,
-                                                  eos_token_id=self.tokenizer.eos_token_id,
-                                                  pad_token_id=self.tokenizer.pad_token_id,
-                                                  **self.generation_kwargs)
+                    outputs = self.model.generate(
+                        vision_x=vision_x,
+                        lang_x=tokenized_data.input_ids,
+                        attention_mask=tokenized_data.attention_mask,
+                        force_words_ids=force_words_ids,
+                        num_beams=10,
+                        eos_token_id=self.tokenizer.eos_token_id,
+                        pad_token_id=self.tokenizer.pad_token_id,
+                        **self.generation_kwargs,
+                    )
                 else:
-                    outputs = self.model.generate(vision_x=vision_x,
-                                                  lang_x=tokenized_data.input_ids,
-                                                  attention_mask=tokenized_data.attention_mask,
-                                                  eos_token_id=self.tokenizer.eos_token_id,
-                                                  pad_token_id=self.tokenizer.pad_token_id,
-                                                  **self.generation_kwargs)
+                    outputs = self.model.generate(
+                        vision_x=vision_x,
+                        lang_x=tokenized_data.input_ids,
+                        attention_mask=tokenized_data.attention_mask,
+                        eos_token_id=self.tokenizer.eos_token_id,
+                        pad_token_id=self.tokenizer.pad_token_id,
+                        **self.generation_kwargs,
+                    )
                 outputs = outputs.tolist()
-                complete_output = self.tokenizer.batch_decode(outputs[:], skip_special_tokens=False)
-                generated = self.tokenizer.batch_decode([output[prompt_len:] for output in outputs], skip_special_tokens=True)
+                complete_output = self.tokenizer.batch_decode(
+                    outputs[:], skip_special_tokens=False
+                )
+                generated = self.tokenizer.batch_decode(
+                    [output[prompt_len:] for output in outputs],
+                    skip_special_tokens=True,
+                )
 
             # 5-3. Save current output
             for prediction, output in zip(generated, complete_output):
@@ -181,7 +235,9 @@ class FlamingoGenInferencer(BaseInferencer):
                 index = index + 1
 
         # 6. Output
-        output_handler.subprocess_write_to_json(output_json_filepath, output_json_filename)
+        output_handler.subprocess_write_to_json(
+            output_json_filepath, output_json_filename
+        )
         if self.accelerator is not None:
             self.accelerator.wait_for_everyone()
         output_handler.merge_to_main_process(output_json_filepath, output_json_filename)
